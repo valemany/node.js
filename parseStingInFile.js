20a96ab594f0a3248e91608f2e4c7be9f1bb6037
valemany <ricardo.alemany@gmail.com>
2017-03-16 01:03:01 -0400

    removing comment sample

--- parseStingInFile.js
+++ parseStingInFile.js
@@ -1,12 +1,3 @@
-// var fs = require('fs');
-// fs.readFile('file.txt', function(err, data) {
-//     if(err) throw err;
-//     var array = data.toString().split("\n");
-//     for(i in array) {
-//         console.log(array[i]);
-//     }
-// });
-
 var fs = require('fs');
 
 fs.readFile('file.txt', function(err, data) {
