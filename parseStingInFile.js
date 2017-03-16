d73f056742b83fb0dd21d5f094e6d2a58bc3313b
Victor <valemany@shutterstock.com>
2017-03-16 01:00:54 -0400

    node.js parsing exercise

---
+++ parseStingInFile.js
@@ -0,0 +1,31 @@
+// var fs = require('fs');
+// fs.readFile('file.txt', function(err, data) {
+//     if(err) throw err;
+//     var array = data.toString().split("\n");
+//     for(i in array) {
+//         console.log(array[i]);
+//     }
+// });
+
+var fs = require('fs');
+
+fs.readFile('file.txt', function(err, data) {
+  if (err) throw err;
+  var array = data.toString().split("\n");
+  var duplicatedEntries = checkDuplicates(array);
+  var totalDuplicatedValues = duplicatedEntries.length;
+  console.log(duplicatedEntries);
+  console.log(totalDuplicatedValues);
+});
+
+// function that checks duplicates in an array and returns an array with duplicated values
+function checkDuplicates(array) {
+  var results = [];
+  var sortedArray = array.slice().sort();
+  for (i=0; i < array.length; i++) {
+    if (sortedArray[i + 1] === array[i]) {
+      results.push(array[i]);
+    }
+  }
+  return results;
+};
