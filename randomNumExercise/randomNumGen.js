19172c3d8dff5a8957069ca9c811358083bc66e6
Victor Alemany <ricardo.alemany@gmail.com>
2023-11-06 16:46:05 -0500

    Delete randomNumExercise directory

--- randomNumExercise/randomNumGen.js
+++
@@ -1,7 +0,0 @@
-function randomNumber() {
-  var date = new Date();
-  var randomNumber = (+date) + (date.getSeconds());
-  console.log(randomNumber.toString().substring(randomNumber.toString().length -4));
-}
-
-randomNumber()
